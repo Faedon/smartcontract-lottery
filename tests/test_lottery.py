3629f18897809248932199cb134b8c40fce8cf9f
Faedon <emrekaan1999@gmail.com>
2022-04-29 05:10:58 +0300

    v2

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,9 +1,12 @@
-from brownie import accounts, config, network
+from brownie import Lottery, accounts, config, network
 from web3 import Web3
-from scripts.helpful_scripts import deploy_lottery
 
 
 def test_get_enterance_fee():
-    lottery = deploy_lottery()
+    account = accounts[0]
+    lottery = Lottery.deploy(
+        config["networks"][network.show_active()]["eth_usd_price_feed"],
+        {"from": account},
+    )
     assert lottery.getEntranceFee() > Web3.toWei(0.016, "ether")
     assert lottery.getEntranceFee() < Web3.toWei(0.020, "ether")
